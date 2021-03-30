0ae693a5e5fb8d70cec44ba43fc9e1a47a4be8ab
Alexandre Medeiros <Altux@users.noreply.github.com>
2021-03-30 23:22:20 +0200

    Update set-french.sh

--- scripts/set-french.sh
+++ scripts/set-french.sh
@@ -3,4 +3,4 @@ sudo apt-get install language-pack-fr -y
 sudo update-locale "LANG=fr_FR.UTF-8"
 sudo locale-gen --purge "fr_FR.UTF-8"
 sudo dpkg-reconfigure --frontend noninteractive locales
-sudo shutdown -r 2
+
